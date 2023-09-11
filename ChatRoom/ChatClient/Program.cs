d87acb235f74c7510cbfebf7ebd695928cbface5
JanikaE <2272253314@qq.com>
2023-09-11 10:47:40 +0800

    ChatRoom小改

--- ChatRoom/ChatClient/Program.cs
+++ ChatRoom/ChatClient/Program.cs
@@ -3,114 +3,121 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 
-class Program
+namespace ChatClient
 {
-    static Socket client;
-    static string name;
-
-    static void Main(string[] args)
+    public static class Program
     {
-        CreateClient();
+        private static Socket? client;
+        private static string? name;
 
-        new Thread(() =>
+        public static void Main()
         {
-            Thread.Sleep(500);
-            SendMessage();
-        }).Start();
+            CreateClient();
 
-        new Thread(() =>
-        {
-            Thread.Sleep(500);
-            ReceiveMessage();
-        }).Start();
-
-    }
-
-    static void CreateClient()
-    {
-        try
-        {
-            Console.WriteLine("设置名字：");
-            name = Console.ReadLine();
-
-            IPAddress iPAdress = new IPAddress(new byte[] { 127, 0, 0, 1 });
-            int port = 8000;
-            IPEndPoint iPEndPoint = new IPEndPoint(iPAdress, port);
-            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            client.Connect(iPEndPoint);
-            Console.WriteLine("已经连接上服务器");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-            Console.ReadKey();
-            Process.GetCurrentProcess().Close();
-        }
-    }
-
-    /// <summary>
-    /// 发送消息
-    /// </summary>
-    static void SendMessage()
-    {
-        try
-        {
-            while (true)
+            new Thread(() =>
             {
-                Console.WriteLine("等待发送信息，回车->发送，close->断开连接");
-                string s = Console.ReadLine();
-                client.Send(Encoding.Default.GetBytes(name + ":" + s));
-                Console.WriteLine("发送成功！\n");
+                Thread.Sleep(500);
+                SendMessage();
+            }).Start();
 
-                if (s == "close")
-                {
-                    client.Close();
-                    break;
-                }
+            new Thread(() =>
+            {
+                Thread.Sleep(500);
+                ReceiveMessage();
+            }).Start();
+
+        }
+
+        private static void CreateClient()
+        {
+            try
+            {
+                Console.WriteLine("设置名字：");
+                name = Console.ReadLine();
+
+                IPAddress iPAdress = new(new byte[] { 127, 0, 0, 1 });
+                int port = 8000;
+                IPEndPoint iPEndPoint = new(iPAdress, port);
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                client.Connect(iPEndPoint);
+                Console.WriteLine("已经连接上服务器");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                Process.GetCurrentProcess().Close();
             }
         }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-    }
 
-    /// <summary>
-    /// 接收消息
-    /// </summary>
-    static void ReceiveMessage()
-    {
-        try
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        private static void SendMessage()
         {
-            while (true)
+            try
             {
-                byte[] buffer = new byte[1024 * 1024 * 8];
-                int length = client.Receive(buffer);
-                string msg = Encoding.Default.GetString(buffer, 0, length);
-                if (buffer.Length > 0)
+                while (true)
                 {
-                    if (msg.StartsWith(name + ":"))
+                    Console.WriteLine("等待发送信息，回车->发送，close->断开连接");
+                    string? s = Console.ReadLine();
+                    client?.Send(Encoding.Default.GetBytes(name + ":" + s));
+                    Console.WriteLine("发送成功！\n");
+
+                    if (s == "close")
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
+                        client?.Close();
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        private static void ReceiveMessage()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 8];
+                    if (client == null)
+                    {
+                        return;
+                    }
+                    int length = client.Receive(buffer);
+                    string msg = Encoding.Default.GetString(buffer, 0, length);
+                    if (buffer.Length > 0)
+                    {
+                        if (msg.StartsWith(name + ":"))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        Console.WriteLine(msg);
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("断开连接");
+                        break;
                     }
-                    Console.WriteLine(msg);
-                    Console.ForegroundColor = ConsoleColor.White;
+                    Thread.Sleep(100);
                 }
-                else
-                {
-                    Console.WriteLine("断开连接");
-                    break;
-                }
-                Thread.Sleep(100);
             }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
