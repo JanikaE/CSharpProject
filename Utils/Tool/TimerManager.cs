66f1993a59119d61c968ad9e7bdb95f2ed1b9659
JanikaE <2272253314@qq.com>
2023-09-20 17:14:50 +0800

    添加TimerManager

---
+++ Utils/Tool/TimerManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Timers;
+
+namespace Utils.Tool
+{
+    public class TimerManager
+    {
+        /// <summary>
+        /// 添加定时任务
+        /// </summary>
+        /// <param name="dateTime">指定触发时间，若已经过了时间则立即触发</param>
+        public static Timer AddTimerManager(DateTime dateTime, Action action)
+        {
+            long delay = dateTime.Ticks - DateTime.Now.Ticks;
+            if (delay < 0)
+            {
+                delay = 0;
+            }
+            return AddTimerEvent(delay, action);
+        }
+
+        /// <summary>
+        /// 添加延时任务
+        /// </summary>
+        /// <param name="delay">延时（毫秒），若小于0则立即触发</param>
+        public static Timer AddTimerEvent(long delay, Action action)
+        {
+            if (delay < 0)
+            {
+                delay = 0;
+            }
+            Timer timerRun = new()
+            {
+                AutoReset = false,
+                Interval = delay
+            };
+            timerRun.Elapsed += delegate
+            {
+                action?.Invoke();
+                timerRun.Dispose();
+            };
+            timerRun.Start();
+            return timerRun;
+        }
+    }
+}
