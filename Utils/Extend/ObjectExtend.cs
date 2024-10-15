56e13b9d40934d8a6389c173f11567c76e219b5a
JanikaE <2272253314@qq.com>
2024-10-15 18:10:55 +0800

    object.Clone

---
+++ Utils/Extend/ObjectExtend.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Utils.Extend
+{
+    public static class ObjectExtend
+    {
+        public static object? Clone(this object obj)
+        {
+            object? newObj = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(obj));
+            return newObj;
+        }
+    }
+}
