202fafdd6daee6242ba8275eb6e936fd2f5585f1
JanikaE <2272253314@qq.com>
2024-11-19 15:55:15 +0800

    Trace

---
+++ Utils/Sample/DebugSample.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Utils.Sample
+{
+    public class DebugSample
+    {
+        public static void WriteTrace(string str)
+        {
+            Trace.TraceInformation(str);
+        }
+
+        public static void WriteDebug(string str)
+        {
+            Debug.Write(str);
+        }
+    }
+}
